b8852b88d23961f28a6ac0213e566f8dbecf7503
ardaaskinn01 <ardaaskindm@gmail.com>
2026-04-09 19:07:42 +0300

    FIXED: Resolved iOS archive failure caused by syntax error in main.dart. Cleaned up initialization flow to prevent white screen and loading hangs.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
 import UIKit
+import UserNotifications
+import alarm
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +10,12 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+    }
+    SwiftAlarmPlugin.registerBackgroundTasks()
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
