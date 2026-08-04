1273af9dbf92fb477b3f52fcd97988798971a31b
ardaaskinn01 <ardaaskindm@gmail.com>
2026-08-04 17:59:17 +0300

    new updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     defaultConfig {
         applicationId = "com.cervus.alarmly"
         minSdk = 24
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
